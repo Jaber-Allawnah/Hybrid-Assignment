3a9fe1e8b803eeeff1277c532876f84d87f46b98
Jaber Allawnah <s12112071@stu.najah.edu>
2026-03-03 21:22:58 +0200

    Refactor App component to use SafeAreaProvider

--- App.js
+++ App.js
@@ -1,22 +1,23 @@
 import React from "react";
 import { StyleSheet } from "react-native";
-import { SafeAreaView } from "react-native";
+import { SafeAreaProvider, SafeAreaView } from "react-native-safe-area-context";
 import { WebView } from "react-native-webview";
-
-const App=()=> {
+//expo suggests to use SafeAreaProvider to avoid the web content entering the safeareas on Android because it was not working without it 
+export default function App() {
   return (
-    <SafeAreaView style={styles.container}>
-      <WebView style={styles.webview} source={{ uri:"https://www.google.com" }}/>
-    </SafeAreaView>
+    <SafeAreaProvider>
+      <SafeAreaView style={styles.container} edges={["top", "bottom"]}>
+        <WebView source={{ uri: "https://www.google.com" }} style={styles.webview} />
+      </SafeAreaView>
+    </SafeAreaProvider>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,//without it safearea will have 0 width and height so it is needed to display
+  container: { 
+    flex: 1 //without it safearea will have 0 width and height so it is needed to display
   },
-  webview: {
-    flex: 1,//take 100% width and 100% height
-  },
+  webview: { 
+    flex: 1//take 100% width and 100% height
+    },
 });
-export default App;
